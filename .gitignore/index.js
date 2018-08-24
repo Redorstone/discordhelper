ca75d66092aee96621b7fa23129c8d2e27fd4da6
Redorstone <35196125+Redorstone@users.noreply.github.com>
2018-08-24 20:35:20 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,26 @@
 const Discord = require("discord.js");
 
+var PREFIX = "!";
+
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setGame("Vous surveille, !help");
     console.log("bot connected");
+});
+
+bot.on("message", async function(message)) {
+    if (message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.sybstring(PREFIX.length).split("");
+
+    switch(args[0].toLowerCase()) {
+        case "invite":
+        message.chanel.send("test1");
+        break;
+    }
 });
 
 bot.login("NDc1MzU5OTk4MzU3Mjc0NjI0.DmHkww.OXJN_uTUppcZpfHHH3NWfixCH4A");
